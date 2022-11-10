c72fe4d4ecb2cce9f657b6bb45d540e8c0863a90
Matias Simon <sm.simonmatias@gmail.com>
2022-11-10 17:17:08 -0300

    se modifican variables

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
 // si la compra supera los 2000 pesos, se aplica un 10% de descuento
 
 
-// let producto = parseInt(prompt(`Hola ${nombre} escoge el producto que deseas comprar: 1.Velas - 2.Fanales - 3.Sahumerios - 4-Velones`))
-
-// let totalCompra = 0
-// let seguirComprando = true
-// let decision
-// while (seguirComprando === true) {
-//     if (producto === 1) {
-//         totalCompra = totalCompra + 500
-//     } else if (producto === 2) {
-//         totalCompra = totalCompra + 1000
-//     } else if (producto === 3) {
-//         totalCompra = totalCompra + 800
-//     } else if (producto === 4) {
-//         totalCompra = totalCompra + 1500
-//     } else {
-//         producto = parseInt(prompt("Escoge un producto de la lista : 1. Velas - 2. Fanales - 3. Sahumerios - 4. - Velones"))
-//         continue
-//     }
-//     decision = parseInt(prompt("Quieres seguir comprando? 1.SI - 2.NO"))
-//     if (decision === 1) {
-//         producto = parseInt(prompt("Escoge el producto que deseas comprar: 1.Velas - 2.Fanales - 3.Sahumerios - 4-Velones"))
-//     } else {
-//         seguirComprando = false
-//     } 
-// }
-
-
-
-
-
-
 let nombre = prompt("Bienvenid@ a mi sitio de Attrezzos, cual es tu nombre?")
 
 
@@ -132,6 +101,6 @@ if (precioCarrito >= 2000) {
 
 } else {
 
-    alert(`${nombre} El total de tu compra es ${precioCarrito}`)
+    alert(`${nombre} Gracias por pensar en mis attrezzos`)
 }
 console.table(productos)
