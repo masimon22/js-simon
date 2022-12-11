64ea3d5a20f11cd069f1b2fe80d0e77bec31e41b
Matias Simon <sm.simonmatias@gmail.com>
2022-12-11 20:25:03 -0300

    se trabaja sobre el proyecto final, se agrega interaccion con DOM, JSON, MODAL, ETC

--- index.js
+++ index.js
@@ -1,93 +1,191 @@
+const divProductos = document.getElementById("divProductos")
+
 
 const productosArray = [];
 
-class Producto {
-    constructor(id, nombre, precio, stock, imagen) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-        this.imagen = imagen;
-    }
-    restaStock() {
-        this.stock = this.stock - 1;
-        console.log(`El stock de ${this.nombre} ha sido actualizado`)
-    }
+
+const consultarProductos = async () => {
+    let res = await fetch("./json/productos.json")
+    let response = await res.json()
+    return response
 
 };
+consultarProductos().then(productos => {
 
-const velas = new Producto(1, "velas", 500, 100, src = "images/vela.jpg");
-productosArray.push(velas)
-const fanales = new Producto(2, "difusor", 1000, 50, src = "images/difusor.jpg");
-productosArray.push(fanales)
-const sahumerios = new Producto(3, "Sahumerios", 800, 100, src = "images/sahumerio.jpg");
-productosArray.push(sahumerios)
-const velones = new Producto(4, "maceta", 1500, 50, src = "images/maceta.jpg");
-productosArray.push(velones)
+    productos.forEach(producto => {
+        productosArray.push(producto)
+        divProductos.innerHTML += `
+                  <div id= ${producto.id} class="card cardProducto">
+                      <img src="./images/${producto.img}" class="card-img-top" alt="...">
+                          <div class="card-body">
+                              <h5 class="card-title">${producto.nombre}</h5>
+                              <p class="card-text">Modelo: ${producto.modelo}</p>
+                              <p class="card-text">Marca: ${producto.marca}</p>
+                              <p class="card-text">Precio:$ ${producto.precio}</p>
+                              
+                              <button id= ${producto.id} class="btn btn-primary">Agregar al carrito</button>
+                          </div>
+                  </div>
+                  `
+    })
 
-// console.table(productosArray)
+    const botonAgregar = document.querySelectorAll(".btn-primary")
 
-const divProductos = document.getElementById("divProductos");
 
-productosArray.forEach(producto => {
-    divProductos.innerHTML += `
-    <div id="${producto.id}" class="card cardProd" style="width: 18rem;">
-    <img src="${producto.imagen}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${producto.nombre}</h5>
-      <p class="card-text">$${producto.precio}.-</p>
-      <button id="${producto.id}" class="btn btn-primary">AGREGAR</button>
-    </div>
-  </div>
 
-`
+
+    botonAgregar.forEach(boton => {
+        boton.onclick = () => {
+            // console.log("hiciste click")
+            const productoSeleccionado = productosArray.find(prod => prod.id === parseInt(boton.id))
+            // console.log(productoSeleccionado)
+            const productoCarrito = { ...productoSeleccionado, cantidad: 1 };
+            // console.log(productoSeleccionado, productoCarrito)
+            const indexCarrito = carrito.findIndex(prod => prod.id === productoCarrito.id)
+            if (indexCarrito === -1) {
+                carrito.push(productoCarrito)
+
+            } else {
+                carrito[indexCarrito].cantidad++
+            }
+            console.table(carrito)
+
+            // localStorage.setItem("carrito", JSON.stringify(carrito))
+
+            Toastify({
+                text: "Producto agregado al carrito",
+                duration: 1500,
+                position: "center",
+                style: {
+                    background: "rgb(171, 181, 181)",
+                }
+
+            }).showToast();
+
+            mostrarCarrito()
+        }
+    })
+
 })
 let carrito
+
+document.addEventListener("DOMContentLoaded", () => {
+    carrito = JSON.parse(localStorage.getItem("carrito")) || []
+    mostrarCarrito()
+})
+
 const carritoStorage = JSON.parse(localStorage.getItem("carrito"))
 if (carritoStorage) {
     carrito = carritoStorage
 } else {
     carrito = []
 }
-
-const botonAgregar = document.querySelectorAll(".btn-primary")
-// console.log(botonAgregar);
-
-botonAgregar.forEach(boton => {
-    boton.onclick = () => {
-        const productoSeleccionado = productosArray.find(prod => prod.id === parseInt(boton.id))
-        
-        // console.log(productoSeleccionado);
-
-        const productoCarrito = { ...productoSeleccionado, cantidad: 1 };
-        // console.log(productoSeleccionado, productoCarrito);
-
-        const indexCarrito = carrito.findIndex(prod => prod.id === productoCarrito.id)
-        if (indexCarrito === -1) {
-            carrito.push(productoCarrito)
+// console.log(productosArray)
 
 
-        } else {
-            carrito[indexCarrito].cantidad++
-        }
 
-        console.table(carrito)
+const mostrarCarrito = () => {
+    const modalBody = document.querySelector(".modal .modal-body")
+    modalBody.innerHTML = ""
+    carrito.forEach((prod) => {
+        const { id, img, nombre, cantidad, precio } = prod
+        // console.log(prod)
+        modalBody.innerHTML += `
+        <div class="carritoModal">
+           
+            <div>
+                <p>Producto: ${nombre} </p>
+                <p>precio: $${precio} </p>
+                <p>Cantidad: ${cantidad}</p>
+                    
+                <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+            </div>
+        </div>
+`
+    })
 
-        localStorage.setItem("carrito", JSON.stringify(carrito))
+    if (carrito.length === 0) {
+        modalBody.innerHTML = `
+        <p class= "text-center text-primary parrafo"> El carrito esta vacio! </p>
+        `
     }
+    guardarStorage()
+    carritoContenedor.textContent = carrito.length
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+}
+function eliminarProducto(id) {
+    const carritoId = id
+    carrito = carrito.filter((prod) => prod.id !== carritoId)
+    console.log(carrito)
+    mostrarCarrito()
+}
+
+function guardarStorage() {
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+
+const carritoContenedor = document.querySelector("#carritoContenedor")
+const vaciarCarrito = document.querySelector("#vaciarCarrito")
+const precioTotal = document.querySelector("#precioTotal")
+const procesarCompra = document.querySelector("#procesarCompra")
+const botonFinalizar = document.querySelector("#finalizar");
+
+
+vaciarCarrito.addEventListener("click", () => {
+    carrito.length = []
+    mostrarCarrito()
+})
+procesarCompra.addEventListener("click", () => {
+    if (carrito.length === 0) {
+        Swal.fire({
+            title: "Tu carrito esta vacio!",
+            text: "Elije un producto para continuar",
+            icon: "error",
+            confirmButtonText: "aceptar",
+        })
+    } else {
+        const valores = carrito.map(prod => prod.precio * prod.cantidad)
+        let totalCompra = 0
+        valores.forEach(valor => {
+            totalCompra += valor
+        })
+        Swal.fire(`El total de tu compra es: $${totalCompra}`)
+        carrito.length = []
+        mostrarCarrito()
+    }
+
 })
 
-const botonFinalizar = document.getElementById("finalizar");
+
 botonFinalizar.onclick = () => {
     const valores = carrito.map(prod => prod.precio * prod.cantidad)
     let totalCompra = 0
     valores.forEach(valor => {
         totalCompra += valor
     })
-    console.log(totalCompra)
+    if (carrito.length === 0) {
+        Swal.fire({
+            title: "Tu carrito esta vacio!",
+            text: "Elije un producto para continuar",
+            icon: "error",
+            confirmButtonText: "aceptar",
+        })
+    } else {
+        Swal.fire(`Gracias por tu compra, el total es: $ ${totalCompra}.-`)
+        localStorage.clear()
+        carrito.length = []
 
-    let valorCompra = `El total de tu compra es: $ ${totalCompra}.-`
+    }
+    mostrarCarrito()
 
-    console.log(valorCompra)
+}
 
-}   
+
+
+
+
+
+
+
+
